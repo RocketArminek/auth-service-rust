7d6a64b40d2fa4972839b5e633fbc7e931c3c67a
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2025-01-14 21:34:19 +0100

    chore: remove access token and refresh token from schema

--- migrations/mysql/20250114200631_create_session_table.up.sql
+++ migrations/mysql/20250114200631_create_session_table.up.sql
@@ -2,12 +2,10 @@
 CREATE TABLE sessions (
     id BINARY(16) PRIMARY KEY,
     user_id BINARY(16) NOT NULL,
-    access_token TEXT NOT NULL,
     created_at DATETIME NOT NULL,
     expires_at DATETIME NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_sessions_user_id ON sessions(user_id);
-CREATE INDEX idx_sessions_access_token ON sessions(access_token(100));
 CREATE INDEX idx_sessions_expires_at ON sessions(expires_at);
