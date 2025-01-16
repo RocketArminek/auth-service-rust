e99a15f807d59aee4e78645bab91631bb1a0df73
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2025-01-16 18:14:17 +0100

    chore: adjust logs

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -533,8 +533,6 @@ fn setup_logging(config: &Configuration) {
         .with_target(true)
         .with_thread_ids(true)
         .with_thread_names(true)
-        .with_file(true)
-        .with_line_number(true)
         .with_max_level(config.app().log_level())
         .json()
         .init();
