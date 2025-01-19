15e2545c3557e664a329ccf7aba282340abe024a
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2025-01-19 11:48:38 +0100

    chore: remove not used util

--- tests/acceptance/utils.rs
+++ tests/acceptance/utils.rs
@@ -5,35 +5,6 @@ use auth_service::domain::role::Role;
 use auth_service::domain::user::{PasswordHandler, User};
 use crate::utils::context::AcceptanceTestContext;
 
-pub async fn create_verified_user(c: &AcceptanceTestContext) -> (User, String) {
-    let mut user = User::now_with_email_and_password(
-        String::from("user@test.com"),
-        String::from("User#pass1"),
-        Some(String::from("User")),
-        Some(String::from("Test")),
-        Some(true),
-    )
-        .unwrap();
-    user.hash_password(&SchemeAwareHasher::default()).unwrap();
-
-    let role = Role::now("USER".to_string()).unwrap();
-    c.role_repository.save(&role).await.unwrap();
-    user.add_role(role);
-    c.user_repository.save(&user).await.unwrap();
-
-    let response = c
-        .server
-        .post("/v1/login")
-        .json(&json!({
-            "email": "user@test.com",
-            "password": "User#pass1",
-        }))
-        .await;
-
-    let body = response.json::<LoginResponse>();
-    (user, body.access_token.value)
-}
-
 pub async fn create_admin_with_token(c: &AcceptanceTestContext) -> (User, String) {
     let mut admin = User::now_with_email_and_password(
         String::from("admin@test.com"),
